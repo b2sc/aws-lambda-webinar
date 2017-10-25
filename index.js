670e049de54b6c58f2c975c4cc39ae446e11ed1d
demsnvp7 <demsnvp7@M-GL0ZLX1.toronto.redknee.com>
2017-10-25 14:55:49 +0200

    handle parameter

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 //   var contents = fs.readFileSync("public/index.html");
    context.succeed({
      statusCode: 200,
-     body: "hello bb: " + JSON.stringify(event),
+     body: "hello bb: a=" + event.queryStringParameters.a + " all=" + JSON.stringify(event),
      headers: {'Content-Type': 'text/html'}
    });
  };
