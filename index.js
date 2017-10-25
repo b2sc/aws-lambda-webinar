4ae68a4ba7a013b0d82bf8441035ee8752bea9b8
demsnvp7 <demsnvp7@M-GL0ZLX1.toronto.redknee.com>
2017-10-25 14:39:07 +0200

    debug event

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 //   var contents = fs.readFileSync("public/index.html");
    context.succeed({
      statusCode: 200,
-     body: "hello bb",
+     body: "hello bb: " + event,
      headers: {'Content-Type': 'text/html'}
    });
  };
