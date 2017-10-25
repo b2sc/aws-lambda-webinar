eac50cee687fc1aa47efac87a4e328c5a48b3ca8
demsnvp7 <demsnvp7@M-GL0ZLX1.toronto.redknee.com>
2017-10-25 14:49:01 +0200

    debug json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 //   var contents = fs.readFileSync("public/index.html");
    context.succeed({
      statusCode: 200,
-     body: "hello bb: " + event,
+     body: "hello bb: " + JSON.stringify(event),
      headers: {'Content-Type': 'text/html'}
    });
  };
