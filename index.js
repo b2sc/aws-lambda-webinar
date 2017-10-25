8cfdcdaf107563ce549c7fbe690905642746e2c4
demsnvp7 <demsnvp7@M-GL0ZLX1.toronto.redknee.com>
2017-10-25 15:16:42 +0200

    debug context 2

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var fs = require('fs');
 //   var contents = fs.readFileSync("public/index.html");
    context.succeed({
      statusCode: 200,
-     body: "hello bb: a=" + event.queryStringParameters.a + " event=" + JSON.stringify(event) + " context=" + JSON.stringify(context),
+     body: "hello bb: a=" + "event.queryStringParameters.a" 
+     	+ " event=" + JSON.stringify(event) 
+     	+ " context=" + JSON.stringify(context),
      headers: {'Content-Type': 'text/html'}
    });
  };
