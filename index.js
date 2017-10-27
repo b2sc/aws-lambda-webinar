3de6fa76b5d1e30f8e7e7908d4b14b8c4fb14b80
Björn-Bernhard Schad <bjoern-bernhard.schad@redknee.com>
2017-10-27 17:49:16 +0200

    deliver content

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
 var fs = require('fs');
 
  exports.get = function(event, context) {
-//   var contents = fs.readFileSync("public/index.html");
-   context.succeed({
+   var contents = fs.readFileSync("public/index.html");
+/*   context.succeed({
      statusCode: 200,
      body: "hello bb: a=" + "event.queryStringParameters.a" 
      	+ " event=" + JSON.stringify(event) 
      	+ " context=" + JSON.stringify(context),
+     headers: {'Content-Type': 'text/html'}
+   });
+*/
+   context.succeed({
+     statusCode: 200,
+     body: contents.toString(),
      headers: {'Content-Type': 'text/html'}
    });
  };
